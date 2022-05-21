c6a72bdd3b76b21bbb7dadb8b507df82e77af6ed
ibadfawa <85802489+ibadfawa@users.noreply.github.com>
2022-05-22 05:14:11 +0800

    Update MainActivity.java

--- app/src/main/java/com/kuro/signreader/MainActivity.java
+++ app/src/main/java/com/kuro/signreader/MainActivity.java
@@ -20,7 +20,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
-import 
+
 import android.support.annotation.NonNull;
 
 public class MainActivity extends Activity {
